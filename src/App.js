0d4f1d19419723dd7234450cb916ad67fdd18fd0
NicksyRop <nicksonkipkorir25@gmail.com>
2022-02-04 14:13:56 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import FormInput from "./components/FormInput";
+import "./index.css";
+import "./styles.css";
 
-function App() {
+function App(props) {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      placeholder: "Username",
+      label: "Username",
+      type: "text",
+      errorMessage:
+        "Should be 3-16 long and shouldnt include special characters",
+      required: true,
+      pattern: "^[A-Za-z0-9]{3,6}$",
+    },
+    {
+      id: 2,
+      name: "email",
+      placeholder: "Email",
+      label: "Email",
+      type: "email",
+      errorMessage: "Should be a valid email address !",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "birthday",
+      placeholder: "Birthday",
+      label: "Birthday",
+      type: "date",
+      required: true,
+    },
+    {
+      id: 4,
+      name: "password",
+      placeholder: "Password",
+      label: "Password",
+      type: "text",
+      errorMessage:
+        "Password should contain 8-20 characters include atleast 1 letter , 1 number and one special character",
+      required: true,
+      pattern: `^(?=.*[a-zA-Z])(?=.*d)(?=.*[!@#$%^&*()_+])[A-Za-zd][A-Za-zd!@#$%^&*()_+]{8,20}$`,
+    },
+    {
+      id: 5,
+      name: "confirm_password",
+      placeholder: "Confirm_password",
+      label: "Password",
+      type: "password",
+      errorMessage: "Password dont match",
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  console.log(values);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/FormInput.js
@@ -0,0 +1,13 @@
+import "../styles.css";
+const FormInput = (props) => {
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+  return (
+    <div className="formInput">
+      <label>{label}</label>
+      <input {...inputProps} onChange={onChange} />
+      <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default FormInput;
